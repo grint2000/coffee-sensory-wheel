9a46bd9944569726b588f3c752e8bbe873a36a4a
grint2000 <107740478+grint2000@users.noreply.github.com>
2025-06-15 05:26:48 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,8 +3,10 @@ const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
+  './extras.js',
   './icons/icon-192.png',
   './icons/icon-512.png',
+  './icons/header-logo.png',
   'https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css',
   'https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.5.2/css/all.min.css',
   'https://cdn.jsdelivr.net/npm/chart.js@4.4.3/dist/chart.umd.min.js',
@@ -17,6 +19,15 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+  return self.clients.claim();
 });
 self.addEventListener('fetch', event => {
   event.respondWith(
